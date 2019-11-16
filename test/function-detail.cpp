8dd5910767bbe4e084a10cd1ac805f7ea9138b23
Dmitry Arkhipov <grisumbras@gmail.com>
2019-11-16 21:39:03 +0300

    refactor

--- test/function-detail.cpp
+++ test/function-detail.cpp
@@ -16,6 +16,11 @@ class a_base_class
 {};
 
 
+struct simple_pointer {
+  using element_type = void;
+};
+
+
 } // namespace
 
 
@@ -26,50 +31,22 @@ static_assert(
       mp_list<int(int) &, int(int) const&, int(int) &&, int(int) const&&>>);
 
 static_assert(
-  xaos::detail::trait_for_overload<xaos::function_traits, int(int) &>::value);
-static_assert(!xaos::detail::trait_for_overload<
-              xaos::function_traits,
-              int(int) const&>::value);
+  xaos::detail::trait_for_ref_kind<xaos::function_traits, int&>::value);
+static_assert(
+  !xaos::detail::trait_for_ref_kind<xaos::function_traits, int const&>::value);
 
+static_assert(std::is_same_v<
+              xaos::detail::enabled_overloads<int(int), xaos::function_traits>,
+              boost::mp11::mp_list<int(int) &>>);
 static_assert(
   std::is_same_v<
-    xaos::detail::enabled_signature_overloads<int(int), xaos::function_traits>,
-    boost::mp11::mp_list<int(int) &>>);
-static_assert(
-  std::is_same_v<
-    xaos::detail::
-      enabled_signature_overloads<int(int), xaos::const_function_traits>,
+    xaos::detail::enabled_overloads<int(int), xaos::const_function_traits>,
     boost::mp11::mp_list<int(int) const&>>);
 static_assert(
   std::is_same_v<
-    xaos::detail::enabled_signature_overloads<int(int), enable_all>,
+    xaos::detail::enabled_overloads<int(int), enable_all>,
     boost::mp11::
       mp_list<int(int) &, int(int) const&, int(int) &&, int(int) const&&>>);
-
-static_assert(std::is_base_of_v<
-              xaos::detail::function_frontend_part<
-                xaos::basic_function<int(), xaos::function_traits>,
-                false,
-                int() &>,
-              xaos::basic_function<int(), xaos::function_traits>>);
-static_assert(std::is_base_of_v<
-              xaos::detail::function_frontend_part<
-                xaos::basic_function<int(), enable_all>,
-                true,
-                int() &>,
-              xaos::basic_function<int(), enable_all>>);
-static_assert(std::is_base_of_v<
-              xaos::detail::function_frontend_part<
-                xaos::basic_function<int(), enable_all>,
-                true,
-                int() const&>,
-              xaos::basic_function<int(), enable_all>>);
-static_assert(std::is_base_of_v<
-              xaos::detail::function_frontend_part<
-                xaos::basic_function<int(), enable_all>,
-                true,
-                int() &&>,
-              xaos::basic_function<int(), enable_all>>);
 
 static_assert(
   xaos::detail::is_copyability_enabled<xaos::function_traits>::value);
@@ -79,13 +56,13 @@ static_assert(
   !xaos::detail::is_copyability_enabled<xaos::rvalue_function_traits>::value);
 
 static_assert(std::is_base_of_v<
-              xaos::detail::noncopyable_backend_holder<
+              xaos::detail::noncopyable_backend_storage<
                 int(),
                 xaos::function_traits,
                 std::allocator<void>>,
               xaos::function<int()>>);
 static_assert(std::is_base_of_v<
-              xaos::detail::copyable_backend_holder<
+              xaos::detail::copyable_backend_storage<
                 int(),
                 xaos::function_traits,
                 std::allocator<void>>,
@@ -97,3 +74,7 @@ static_assert(std::is_copy_constructible_v<xaos::const_function<int()>>);
 static_assert(std::is_copy_assignable_v<xaos::const_function<int()>>);
 static_assert(!std::is_copy_constructible_v<xaos::rfunction<int()>>);
 static_assert(!std::is_copy_assignable_v<xaos::rfunction<int()>>);
+
+static_assert(xaos::detail::has_pointer_to<int*>::value);
+static_assert(xaos::detail::has_pointer_to<void**>::value);
+static_assert(!xaos::detail::has_pointer_to<simple_pointer>::value);
